c2ca35cac580a2d8907e928df2c3036d2a9da806
Taviii <s21354@pjwstk.edu.pl>
2023-06-07 13:22:11 +0200

    update 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { TodoItem, TodoList } from './todo-list';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  list?: TodoList;
+
+  showCompleted: boolean = false;
+
+  constructor() {
+    this.list = new TodoList('Lista zadań');
+
+    // for (let task of this.list.tasks){
+    //   console.log(task);
+    // }
+  }
+
+  tasksList(): TodoItem[] {
+    if (this.list === undefined)
+      return [];
+    if (this.showCompleted)
+    return  this.list.tasks;
+    else
+    return this.list.tasks.filter(task => !task.completed);
+
+    // function filtrZadanNiekonczonych(task: TodoItem): boolean {
+    //   return task.completed;
+    // }
+    // this.list?.tasks.filter(filtrZadanNiekonczonych);
+
+  }
+
+  tasksCount(): string {
+    const count = this.tasksList().length;
+    switch (count) {
+      case 0: return 'Brak zadań';
+      case 1: return '1 zadanie';
+      case 2: return '2 zadania';
+      case 3: return '3 zadania';
+      case 4: return `${count} zadania`;
+      default: return `${count} zadań`;
+    }
+  }
+
+  addTask(name: string) {
+    this.list?.tasks?.push(new TodoItem(name));
+  }
+
 }
